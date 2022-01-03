ed583d23f0445f0c42af74dfc47fed62aab1bf99
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-03 19:09:53 +0900

    feat : 회원가입 뒷 부분 화면전환 구현

---
+++ EggMeet/EggMeet/SignUp/SignUpCheckTermsOfServiceVC.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpCheckTermsOfServiceVC.swift
+//  EggMeet
+//
+//  Created by 이윤성 on 2022/01/03.
+//
+
+import Foundation
---
+++ EggMeet/EggMeet/SignUp/SignUpSetChannelVC.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpSetChannelVC.swift
+//  EggMeet
+//
+//  Created by 이윤성 on 2022/01/03.
+//
+
+import Foundation
