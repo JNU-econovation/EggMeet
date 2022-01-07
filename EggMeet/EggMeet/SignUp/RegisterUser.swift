b1204a460c6491a451f276707a59bca92067959a
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-07 22:38:03 +0900

    feat : API 전송에 필요한 Parameter 설정

--- EggMeet/EggMeet/SignUp/RegisterUser.swift
+++ EggMeet/EggMeet/SignUp/RegisterUser.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+// JSON으로 보낼 구조체
+struct RegisterUser {
+    let age: String
+    let description: String?
+    let email: String?
+    let location: String?
+    let loginType: String
+    let menteeCategory: String?
+    let menteeDescription: String?
+    let mentorCareer: String?
+    let mentorCategory: String?
+    let mentorDescription: String?
+    let mentorLink: String?
+    let name: String
+    let offlineAvailable: Bool
+    let onlineAvailable: Bool
+    let phoneNumber: String?
+    let pictureIndex: String?
+    let role: String?
+    let sex: String
+    let socialToken: String
+    
+}
