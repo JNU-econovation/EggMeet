e81a9ac854772158341cac8173fc5fd7e7fbd316
asong57 <1998thddk@gmail.com>
2022-01-07 15:10:59 +0900

    feat : 회원가입 네비게이션바 타이틀 추가 완료

--- EggMeet/EggMeet/SignUp/SignUpMentoDetailIntroduceVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpMentoDetailIntroduceVC.swift
@@ -11,11 +11,15 @@ import UIKit
 class SignUpMentoDetailIntroduceVC: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "멘토 상세 소개";
+        self.navigationController?.navigationBar.tintColor = .black
     }
+    
     @IBAction func windSignUpMentorAreaCareerView(_ sender: Any){
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpMentorAreaCareerVC") as? SignUpMentorAreaCareerVC else {return}
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
+    
     @IBAction func skipAndWindSignUpMentorAreaCareerView(_ sender: Any){
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpMentorAreaCareerVC") as? SignUpMentorAreaCareerVC else {return}
         self.navigationController?.pushViewController(nextVC, animated: true)
--- EggMeet/EggMeet/SignUp/SignUpSelfIntroduceVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpSelfIntroduceVC.swift
@@ -11,11 +11,15 @@ import UIKit
 class SignUpSelfIntroduceVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "자기 소개";
+        self.navigationController?.navigationBar.tintColor = .black
     }
+    
     @IBAction func windSignUpMentoAreaSelectionView(_ sender: Any){
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpMentoAreaSelectionVC") as? SignUpMentoAreaSelectionVC else {return}
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
+    
     @IBAction func skipAndWindSignUpMentoAreaSelectionView(_ sender: Any){
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpMentoAreaSelectionVC") as? SignUpMentoAreaSelectionVC else {return}
         self.navigationController?.pushViewController(nextVC, animated: true)
