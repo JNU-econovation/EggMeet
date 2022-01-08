73020b91a6a811ca6b2ba1664c7c2413743f7995
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-09 04:12:28 +0900

    feat : 개인정보처리방침 및 서비스 이용약관 View 생성

---
+++ EggMeet/EggMeet/SignUp/SignUpShowPersonalInfoVC.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpShowPersonalInfoVC.swift
+//  EggMeet
+//
+//  Created by 이윤성 on 2022/01/09.
+//
+
+import Foundation
---
+++ EggMeet/EggMeet/SignUp/SignUpShowTermsOfServiceVC.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpShowTermsOfServiceVC.swift
+//  EggMeet
+//
+//  Created by 이윤성 on 2022/01/09.
+//
+
+import Foundation
