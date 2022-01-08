5bcdfba45a27e11dde4afa73a0ed8c894dcd704d
asong57 <1998thddk@gmail.com>
2022-01-08 22:26:22 +0900

    feat : 닉네임 서버 통신 틀 추가

--- EggMeet/EggMeet/SignUp/SignUpNickNameVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpNickNameVC.swift
@@ -4,7 +4,7 @@
 //
 //  Created by asong on 2022/01/01.
 //
-
+import Alamofire
 import Foundation
 import UIKit
 
@@ -26,6 +26,28 @@ class SignUpNickNameVC: UIViewController {
         print("실행")
         self.navigationController?.pushViewController(nextVC, animated: true)
         
+    }
+    
+    func checkNicknameAvailable(){
+        //접근하고자 하는 URL 정보
+        let URL = "NickNameURL"
+    
+        //위의 URL와 파라미터를 담아서 POST 방식으로 통신하며, statusCode가 200번대(정상적인 통신) 인지 유효성 검사 진행
+        let alamo = AF.request(URL, method: .post, parameters: self.nicknameTextField.text).validate(statusCode: 200..<300)
+        
+        //결과값으로 문자열을 받을 때 사용
+        alamo.responseString() { response in
+            switch response.result
+            {
+            //통신성공
+            case .success(let value):
+                print("value: \(value)")
+                
+            //통신실패
+            case .failure(let error):
+                print("error: \(String(describing: error.errorDescription))")
+            }
+        }
     }
     
 }
