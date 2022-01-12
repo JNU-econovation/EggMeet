b3fd2e2f32e59d1a118794c4995f1ed6bce3caa4
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-12 19:55:22 +0900

    feat : socketIO 구현중

---
+++ EggMeet/EggMeet/Chat/VC/ChatTVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTVC.swift
+//  EggMeet
+//
+//  Created by 이윤성 on 2022/01/12.
+//
+
+import Foundation
+import UIKit
+
+class ChatTVC: UITableViewCell {
+    @IBOutlet weak var chatLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
+
