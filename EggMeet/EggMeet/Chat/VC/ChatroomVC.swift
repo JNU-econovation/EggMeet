98c14465dc53c4fa9cd42c07b6959826df0fa2e2
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-13 16:06:31 +0900

    feat : TextView에 작성한 Text를 전송할 수 있게 함

--- EggMeet/EggMeet/Chat/VC/ChatroomVC.swift
+++ EggMeet/EggMeet/Chat/VC/ChatroomVC.swift
@@ -17,7 +17,7 @@ class ChatroomVC: UIViewController, URLSessionWebSocketDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        chatOpponentNameLabel.text = self.nickname
         let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
         var mainAddress :String = Bundle.main.infoDictionary!["WS_URL"] as? String ?? ""
         let wsURL: String = "ws://" + mainAddress + "/ws/chat"
@@ -26,9 +26,12 @@ class ChatroomVC: UIViewController, URLSessionWebSocketDelegate {
         webSocket = session.webSocketTask(with: url!)
         webSocket?.resume()
     }
+    
+    //
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.messageTextView.endEditing(true)
         }
+    
     override func viewWillDisappear(_ animated: Bool) {
     }
     
@@ -47,7 +50,7 @@ class ChatroomVC: UIViewController, URLSessionWebSocketDelegate {
             chatOpponentNameLabel.text = "\(nickname)"
         }
     }
-    
+
     func ping() {
         webSocket?.sendPing(pongReceiveHandler: { error in
             if let error = error {
@@ -78,6 +81,7 @@ class ChatroomVC: UIViewController, URLSessionWebSocketDelegate {
                 case .data(let data):
                     print("get Data : \(data)")
                 case .string(let message):
+                    // 스트링 데이터를 받아서 내가 보낸건지 판단 후, View 처리 해야함.
                     print("Got String : \(message)")
                 @unknown default:
                     break
