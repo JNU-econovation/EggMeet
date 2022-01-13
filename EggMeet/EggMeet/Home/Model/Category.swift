1202147408fb367455e869241b3794554c92ef73
asong57 <1998thddk@gmail.com>
2022-01-13 16:49:08 +0900

    feat : Home 관련 DTO 작성

---
+++ EggMeet/EggMeet/Home/Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  EggMeet
+//
+//  Created by asong on 2022/01/13.
+//
+
+import Foundation
+
+enum Category: Codable {
+    case PROGRAMMING_PYTHON,
+    PROGRAMMING_JAVA,
+    PROGRAMMING_C,
+    PROGRAMMING_CPP
+}
--- EggMeet/EggMeet/Home/Model/HomeResponseModel.swift
+++ EggMeet/EggMeet/Home/Model/HomeResponseModel.swift
@@ -10,6 +10,20 @@ import Foundation
 struct HomeResponseModel: Codable {
     let age: Int
     let description: String
-    let location: String
-    let menteeCategory: 
+    let location: Location
+    let menteeCategory: Category
+    let menteeDescription: String
+    let menteeRating: Float
+    let mentorCareer: String
+    let mentorDescription: String
+    let mentorGrowthPoint: Int
+    let mentorLink: String
+    let mentorRating: Float
+    let nickname: String
+    let isOfflineAvailable: Bool
+    let isOnlineAvailable: Bool
+    let pictureIndex: Int
+    let sex: Sex
 }
+
+
