42aab8d6f2611186d266b4f216ba54d13231632e
asong57 <1998thddk@gmail.com>
2022-01-13 21:12:51 +0900

    feat : 회원가입 나이 화면 구현

---
+++ EggMeet/EggMeet/SignUp/SignUpSexVC.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpSex.swift
+//  EggMeet
+//
+//  Created by asong on 2022/01/13.
+//
+
+import Foundation
+import UIKit
+
+class SignUpSexVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "나이";
+        self.navigationController?.navigationBar.tintColor = .black
+    }
+}
