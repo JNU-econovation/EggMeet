c180a3ef81955d07799e0b4f9ac699509207d015
asong57 <1998thddk@gmail.com>
2022-01-14 16:49:22 +0900

    feat : 회원가입된 유저는 로그인 후 홈화면으로 이동 코드

--- EggMeet/EggMeet/ViewController.swift
+++ EggMeet/EggMeet/ViewController.swift
@@ -165,7 +165,9 @@ extension ViewController: ASAuthorizationControllerDelegate, Encodable {
                 
                 // 기존 유저 -> 홈 화면 segue
                 if response.response?.statusCode == 200{
-                    self.performSegue(withIdentifier: "HomeVC", sender: nil)
+                    let sb = UIStoryboard(name: "HomeStoryboard", bundle: nil)
+                    let nextVC = sb.instantiateViewController(withIdentifier: "HomeVC") as! HomeVC
+                    self.navigationController?.pushViewController(nextVC, animated: true)
                 // 신규 유저 -> register 204
                 } else{
                     guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpNickNameVC") as? SignUpNickNameVC else {return}
