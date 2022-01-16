5fb905cbde6a0448e7549219c2d87a550c01746c
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-16 17:23:25 +0900

    feat : message 송/수신시 table view cell 에 추가

--- EggMeet/EggMeet/Chat/VC/ChatroomVC.swift
+++ EggMeet/EggMeet/Chat/VC/ChatroomVC.swift
@@ -25,10 +25,10 @@ class ChatroomVC: UIViewController, StompClientLibDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        chatOpponentNameLabel.text = self.opponentNickname
-        chatTableView.delegate = self
-        chatTableView.dataSource = self
-        
+        self.chatOpponentNameLabel.text = self.opponentNickname
+        self.chatTableView.delegate = self
+        self.chatTableView.dataSource = self
+        self.chatTableView.rowHeight = UITableView.automaticDimension
         if !isExistChatRoom(){
             createChatRoom()
         }
@@ -49,7 +49,7 @@ class ChatroomVC: UIViewController, StompClientLibDelegate{
         
     }
     
-    @IBAction func passMessage(_ sender: Any) {
+    @IBAction func passMessage(_ sender: UIButton) {
         // pass Message Logic
         let messageString = messageTextView.text!
         if messageString == ""{
@@ -100,12 +100,22 @@ class ChatroomVC: UIViewController, StompClientLibDelegate{
         NSLog("DESTINATION : \(destination)")
         NSLog("JSON BODY : \(String(describing: jsonBody))")
         NSLog("STRING BODY : \(stringBody ?? "nil")")
+        
+        if let data = stringBody?.data(using: .utf8){
+            let chatJSON = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            let chatContent :chatDto = chatDto(roomId: chatJSON["roomId"] as! Int, writer: chatJSON["writer"] as! String, message: chatJSON["message"] as! String)
+            self.chatContentList.append(chatContent)
+            NSLog("success append chat Content : \(chatContent)")
+            NSLog("chatContentList : \(self.chatContentList)")
+        }
+        self.chatTableView.reloadSections(IndexSet(0...0), with: UITableView.RowAnimation.automatic)
     }
     
     
     func stompClientJSONBody(client: StompClientLib!, didReceiveMessageWithJSONBody jsonBody: String?, withHeader header: [String : String]?, withDestination destination: String) {
          print("DESTINATION : \(destination)")
          print("String JSON BODY : \(String(describing: jsonBody))")
+        
      }
      
      func serverDidSendReceipt(client: StompClientLib!, withReceiptId receiptId: String) {
@@ -151,13 +161,27 @@ class ChatroomVC: UIViewController, StompClientLibDelegate{
 }
 
 extension ChatroomVC: UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chatContentList.count
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("section : \(section)")
+        return self.chatContentList.count
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChatTableViewCell", for: indexPath) as! ChatTableViewCell
-        cell.nicknameLabel.text = self.chatContentList[indexPath.row].writer
-        cell.contentLabel.text = self.chatContentList[indexPath.row].message
+        tableView.rowHeight = UITableView.automaticDimension
+        NSLog("comming cellForRowAt")
+        cell.nicknameLabel?.text = self.chatContentList[indexPath.row].writer
+        cell.contentLabel?.text = self.chatContentList[indexPath.row].message
+        
+        NSLog("cell.nickname.label : \(cell.nicknameLabel.text!)")
+        NSLog("cell.content.label : \(cell.contentLabel.text!)")
+        
+        NSLog("success cell in text writer : \(self.chatContentList[indexPath.row].writer)")
+        NSLog("success cell in text content : \(self.chatContentList[indexPath.row].message)")
         
         return cell
     }
