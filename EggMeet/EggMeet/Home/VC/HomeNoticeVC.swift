f27d86ad0ddd63f82197c83cbbdde823397dc21d
asong57 <1998thddk@gmail.com>
2022-01-17 21:49:02 +0900

    feat : 알림 화면 생성

---
+++ EggMeet/EggMeet/Home/VC/HomeNoticeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeNoticeVC.swift
+//  EggMeet
+//
+//  Created by asong on 2022/01/17.
+//
+
+import Foundation
+import UIKit
+
+class HomeNoticeVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "알림";
+        self.navigationController?.navigationBar.tintColor = .black
+    }
+}
