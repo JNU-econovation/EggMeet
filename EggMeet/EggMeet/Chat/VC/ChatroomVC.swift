f062740768abc2a96f077849241ceb00aad45d03
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-18 12:58:17 +0900

    refactor : delegate 패턴 적용

--- EggMeet/EggMeet/Chat/VC/ChatroomVC.swift
+++ EggMeet/EggMeet/Chat/VC/ChatroomVC.swift
@@ -10,7 +10,11 @@ import UIKit
 import StompClientLib
 import Alamofire
 
-class ChatroomVC: UIViewController, StompClientLibDelegate{
+let CHAT_SECTION_NUM = 1
+let STATUS_OK = 200
+
+
+class ChatroomVC: UIViewController{
     var opponentNickname: String?
     var socketClient = StompClientLib()
     var url = NSURL()
@@ -44,10 +48,6 @@ class ChatroomVC: UIViewController, StompClientLibDelegate{
         socketClient.disconnect()
     }
     
-    @IBAction func stopWebSocket(_ sender: Any) {
-        
-    }
-    
     @IBAction func passMessage(_ sender: UIButton) {
         // pass Message Logic
         let messageString = messageTextView.text!
@@ -79,69 +79,25 @@ class ChatroomVC: UIViewController, StompClientLibDelegate{
         let baseURL = Bundle.main.infoDictionary!["WS_URL"] as? String ?? ""
         let completeURL = "ws://" + baseURL + "/stomp-chat"
         let wsurl = NSURL(string: completeURL)!
-        print(wsurl)
-        NSLog("URL : \(completeURL)")
         socketClient.openSocketWithURLRequest(request: NSURLRequest(url: wsurl as URL), delegate: self as StompClientLibDelegate)
     }
-    
-    // subscribe function
-    func stompClientDidConnect(client: StompClientLib!) {
-        let topic = self.subscribeTopic + "\(chatroomID)"
-        NSLog("\(topic)")
-        print("socket is connected : \(topic)")
-        socketClient.subscribe(destination: topic)
-    }
-    
-    // disconnect
-    func stompClientDidDisconnect(client: StompClientLib!) {
-        NSLog("Socket is Disconnected")
-    }
-    
-    // send 시, 해당 로그를 출력한다.
-    func stompClient(client: StompClientLib!, didReceiveMessageWithJSONBody jsonBody: AnyObject?, akaStringBody stringBody: String?, withHeader header: [String : String]?, withDestination destination: String) {
-        NSLog("DESTINATION : \(destination)")
-        NSLog("JSON BODY : \(String(describing: jsonBody))")
-        NSLog("STRING BODY : \(stringBody ?? "nil")")
-        
-        if let data = stringBody?.data(using: .utf8){
-            let chatJSON = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            let chatContent :chatDto = chatDto(roomId: chatJSON["roomId"] as! Int, writer: chatJSON["writer"] as! String, message: chatJSON["message"] as! String)
-            self.chatContentList.append(chatContent)
-            NSLog("success append chat Con=tent : \(chatContent)")
-            NSLog("chatContentList : \(self.chatContentList)")
-        }
-        self.chatTableView.reloadSections(IndexSet(0...0), with: UITableView.RowAnimation.automatic)
-    }
-    
     
     func stompClientJSONBody(client: StompClientLib!, didReceiveMessageWithJSONBody jsonBody: String?, withHeader header: [String : String]?, withDestination destination: String) {
          print("DESTINATION : \(destination)")
          print("String JSON BODY : \(String(describing: jsonBody))")
         
      }
-     
-     func serverDidSendReceipt(client: StompClientLib!, withReceiptId receiptId: String) {
-         print("Receipt : \(receiptId)")
-     }
-     
-     func serverDidSendError(client: StompClientLib!, withErrorMessage description: String, detailedErrorMessage message: String?) {
-         print("Error : \(String(describing: message))")
-     }
-     
-     func serverDidSendPing() {
-         print("Server Ping")
-     }
     
     // createChatRoom from id
     func createChatRoom(){
-        let id: Int = 1     // chatroom number
+        let id: Int = CHAT_SECTION_NUM     // chatroom number
         let baseURL = Bundle.main.infoDictionary!["WS_URL"] as? String ?? ""
         let postURL = "http://" + baseURL + "/chat/room"
         let param = ChatroomID(id: id)
         
         AF.request(postURL, method: .post, parameters: param, encoder: JSONParameterEncoder.default).response { response in
             debugPrint(response)
-            if response.response?.statusCode == 200 {
+            if response.response?.statusCode == STATUS_OK {
                 self.chatroomID = id
             } else {
                 print("")
@@ -196,20 +152,61 @@ extension ChatroomVC: UITableViewDelegate, UITableViewDataSource{
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChatTVC", for: indexPath) as! ChatTVC
-        NSLog("comming cellForRowAt")
         cell.nicknameLabel?.text = self.chatContentList[indexPath.row].writer
         cell.contentLabel?.text = self.chatContentList[indexPath.row].message
-        
+        printCellDataLog(cell: cell, indexPath: indexPath)
+        return cell
+    }
+    
+    func printCellDataLog(cell: ChatTVC, indexPath: IndexPath){
         NSLog("cell.nickname.label : \(cell.nicknameLabel.text!)")
         NSLog("cell.content.label : \(cell.contentLabel.text!)")
-        
         NSLog("success cell in text writer : \(self.chatContentList[indexPath.row].writer)")
         NSLog("success cell in text content : \(self.chatContentList[indexPath.row].message)")
-        
-        return cell
     }
     
     
 }
 
+extension ChatroomVC: StompClientLibDelegate {
+    // send 시, 해당 로그를 출력한다.
+    func stompClient(client: StompClientLib!, didReceiveMessageWithJSONBody jsonBody: AnyObject?, akaStringBody stringBody: String?, withHeader header: [String : String]?, withDestination destination: String) {
+        NSLog("DESTINATION : \(destination)")
+        NSLog("JSON BODY : \(String(describing: jsonBody))")
+        NSLog("STRING BODY : \(stringBody ?? "nil")")
+        
+        if let data = stringBody?.data(using: .utf8){
+            let chatJSON = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            let chatContent :chatDto = chatDto(roomId: chatJSON["roomId"] as! Int, writer: chatJSON["writer"] as! String, message: chatJSON["message"] as! String)
+            self.chatContentList.append(chatContent)
+            NSLog("success append chat Con=tent : \(chatContent)")
+            NSLog("chatContentList : \(self.chatContentList)")
+        }
+        self.chatTableView.reloadSections(IndexSet(0...0), with: UITableView.RowAnimation.automatic)
+    }
+    
+    // disconnect
+    func stompClientDidDisconnect(client: StompClientLib!) {
+        NSLog("Socket is Disconnected")
+    }
+    
+    // subscribe function
+    func stompClientDidConnect(client: StompClientLib!) {
+        let topic = self.subscribeTopic + "\(chatroomID)"
+        NSLog("\(topic)")
+        print("socket is connected : \(topic)")
+        socketClient.subscribe(destination: topic)
+    }
+    
+    func serverDidSendReceipt(client: StompClientLib!, withReceiptId receiptId: String) {
+        print("Receipt : \(receiptId)")
+    }
 
+    func serverDidSendError(client: StompClientLib!, withErrorMessage description: String, detailedErrorMessage message: String?) {
+        print("Error : \(String(describing: message))")
+    }
+    
+    func serverDidSendPing() {
+        print("Server Ping")
+    }
+}
