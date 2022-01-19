c24f17339b731ef87e9b4b5d346d016d02a1046b
yunseong9999 <dbsdltjd3701@naver.com>
2022-01-20 01:07:38 +0900

    feat : 멘토 경력, 멘티 상세 소개 글자 수 제한 및 카운트 설정

--- EggMeet/EggMeet/SignUp/SignUpMentorAreaCareerVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpMentorAreaCareerVC.swift
@@ -11,40 +11,71 @@ import UIKit
 class SignUpMentorAreaCareerVC : UIViewController, UITextFieldDelegate {
     
     
-    @IBOutlet weak var mentorAreaCareerTextField: UITextView!
+    @IBOutlet weak var mentorAreaCareerTextView: UITextView!
     @IBOutlet var explainLabel: UILabel!
-    
+    @IBOutlet var textViewCountLabel: UILabel!
+
     let ud = UserDefaults.standard
     let mentorAreaCareerKey: String = "mentorAreaCareer"
     
     override func viewDidLoad() {
         buildTextViewStyle()
+        textViewCountLabel.text = "\(mentorAreaCareerTextView.text.count)/500"
         super.viewDidLoad()
     }
     
     // 화면 터치시 키보드 내려감.
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            self.mentorAreaCareerTextField.resignFirstResponder()
+            self.mentorAreaCareerTextView.resignFirstResponder()
         }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
 
     @IBAction func windSignUpSetGrowthPointView(_ sender: Any){
+        if self.mentorAreaCareerTextView.text == "" {
+            showAlert()
+            return
+        }
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpSetGrowthPointVC") as? SignUpSetGrowthPointVC else {return}
-        ud.set(self.mentorAreaCareerTextField.text, forKey: mentorAreaCareerKey)
+        ud.set(self.mentorAreaCareerTextView.text, forKey: mentorAreaCareerKey)
         print(ud.value(forKey: mentorAreaCareerKey))
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
     @IBAction func skipAndWindSignUpSetGrowthPointView(_ sender: Any){
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpSetGrowthPointVC") as? SignUpSetGrowthPointVC else {return}
-        self.mentorAreaCareerTextField.text = ""
+        self.mentorAreaCareerTextView.text = ""
         ud.set("", forKey: mentorAreaCareerKey)
         print(ud.value(forKey: mentorAreaCareerKey))
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
     func buildTextViewStyle(){
-        self.mentorAreaCareerTextField.layer.borderWidth = 1.0
-        self.mentorAreaCareerTextField.layer.borderColor = UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1).cgColor
-        self.mentorAreaCareerTextField.layer.cornerRadius = 12
+        self.mentorAreaCareerTextView.layer.borderWidth = 1.0
+        self.mentorAreaCareerTextView.layer.borderColor = UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1).cgColor
+        self.mentorAreaCareerTextView.layer.cornerRadius = 12
+    }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "주의", message: "내용을 작성해주세요!", preferredStyle: UIAlertController.Style.alert)
+        let cancelButton = UIAlertAction(title: "확인", style: .default , handler: nil)
+        alert.addAction(cancelButton)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension SignUpMentorAreaCareerVC : UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        textViewCountLabel.text = "\(mentorAreaCareerTextView.text.count)/500"
+        if mentorAreaCareerTextView.text.count != 0 {
+            // image 설정
+        }else if mentorAreaCareerTextView.text.count > 500 {
+            mentorAreaCareerTextView.deleteBackward()
+        } else if mentorAreaCareerTextView.text.count == 0 {
+            // image 설정
+        }
     }
 }
