4696e2df1cd8cdca710260e9585d704113fed525
asong57 <1998thddk@gmail.com>
2022-01-21 12:33:13 +0900

    feat : 회원가입 자기솟개 textView 글자수 세기 구현

--- EggMeet/EggMeet/SignUp/SignUpSelfIntroduceVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpSelfIntroduceVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SignUpSelfIntroduceVC: UIViewController {
     @IBOutlet weak var selfIntroduceTextView: UITextView!
+    @IBOutlet weak var textViewCountLabel: UILabel!
     let selfIntroduceKey = "description"
     let ud = UserDefaults.standard
     
@@ -21,6 +22,7 @@ class SignUpSelfIntroduceVC: UIViewController {
         selfIntroduceTextView.layer.borderColor = UIColor.systemGray4.cgColor
         selfIntroduceTextView.layer.cornerRadius = 10
         self.navigationController?.navigationBar.topItem?.title = ""
+        selfIntroduceTextView.delegate = self
     }
     
     @IBAction func windSignUpMentoAreaSelectionView(_ sender: Any){
@@ -43,4 +45,18 @@ class SignUpSelfIntroduceVC: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.selfIntroduceTextView.endEditing(true)
         }
+}
+
+extension SignUpSelfIntroduceVC : UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        self.textViewCountLabel.text = "\(self.selfIntroduceTextView.text.count)/500"
+        if selfIntroduceTextView.text.count != 0 {
+            // enable image
+        } else {
+            // disable image
+        }
+        if selfIntroduceTextView.text.count > 500{
+            self.selfIntroduceTextView.deleteBackward()
+        }
+    }
 }
