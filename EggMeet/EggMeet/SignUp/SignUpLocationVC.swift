c6d925c56baa6ae21dfcd8027de929e21e76b5f4
asong57 <1998thddk@gmail.com>
2022-01-22 16:13:20 +0900

    feat : 회원가입 활동지역 선택 collectionView 구현 세팅

--- EggMeet/EggMeet/SignUp/SignUpLocationVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpLocationVC.swift
@@ -9,9 +9,12 @@ import Foundation
 import UIKit
 import DropDown
 
-class SignUpLocationVC: UIViewController {
+class SignUpLocationVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
     @IBOutlet weak var locationAreaButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var collcectionView: UICollectionView!
+    
+    var list = ["1", "2", "3", "4" ,"5", "6", "7", "8", "9", "10"]
     
     let locationAreaDD: DropDown = DropDown()
     let locationKey = "location"
@@ -19,6 +22,19 @@ class SignUpLocationVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collcectionView.delegate = self
+        self.collcectionView.dataSource = self
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 7
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.sectionInset.top = 9
+        flowLayout.sectionInset.left = 9
+        flowLayout.sectionInset.bottom = 10
+        flowLayout.sectionInset.right = 7
+        flowLayout.itemSize = CGSize(width: 48, height: 18)
+        self.collcectionView.collectionViewLayout = flowLayout
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,4 +68,55 @@ class SignUpLocationVC: UIViewController {
             ud.set(item, forKey: locationKey)
         }
     }
+}
+
+extension SignUpLocationVC {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return list.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LocationCollectionViewCell
+            
+            //cell.backgroundColor = .lightGray
+            cell.locationLabel.text = list[indexPath.row]
+            cell.locationLabel.textColor = .systemGray3
+            //cell.locationLabel.backgroundColor = .yellow
+            
+            return cell
+        }
+}
+
+// cell layout
+extension SignUpLocationVC: UICollectionViewDelegateFlowLayout {
+
+    // 위 아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 7
+    }
+
+    // 옆 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("click index=\(indexPath.row)")
+        let cell = collectionView.cellForItem(at: indexPath) as! LocationCollectionViewCell
+        //selectStartYearPos = indexPath.row
+        
+    }
+
+    /*
+    // cell 사이즈( 옆 라인을 고려하여 설정 )
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let width = collectionView.frame.width / 5 - 1 ///  3등분하여 배치, 옆 간격이 1이므로 1을 빼줌
+        print("collectionView width=\(collectionView.frame.width)")
+        print("cell하나당 width=\(width)")
+        print("root view width = \(self.view.frame.width)")
+
+        let size = CGSize(width: width, height: 18)
+        return size
+    }*/
 }
---
+++ EggMeet/EggMeet/SignUp/View/Cell/LocationCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LocationCollectionViewCell.swift
+//  EggMeet
+//
+//  Created by asong on 2022/01/22.
+//
+
+import Foundation
+import UIKit
+
+class LocationCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var locationLabel: UILabel!
+}
