36e70357865a736caa41bc4af61046630f60bf73
asong57 <1998thddk@gmail.com>
2022-01-23 17:29:06 +0900

    feat : 캘린더 세팅 및 메소드 추가

--- EggMeet/EggMeet/Calendar/VC/NewMentoringScheduleSetVC.swift
+++ EggMeet/EggMeet/Calendar/VC/NewMentoringScheduleSetVC.swift
@@ -7,11 +7,18 @@
 
 import Foundation
 import UIKit
+import FSCalendar
 
 class NewMentoringScheduleSetVC: UIViewController{
+    @IBOutlet weak var calendarView: FSCalendar!
+    
+    let dateFormatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        calendarView.delegate = self
+        calendarView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -20,5 +27,17 @@ class NewMentoringScheduleSetVC: UIViewController{
         self.navigationController?.navigationBar.topItem?.title = ""
         self.navigationItem.title = "신규 멘토링 등록";
         self.navigationController?.navigationBar.isHidden =  false
+    }
+}
+
+extension NewMentoringScheduleSetVC : FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    // 날짜 선택 시 콜백 메소드
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 선택됨")
+    }
+    
+    // 날짜 선택 해제 시 콜백 메소드
+    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 해제됨")
     }
 }
