bd6dad066e4bdd1e972b1862c6e23e4e806920c8
asong57 <1998thddk@gmail.com>
2022-01-27 10:37:20 +0900

    feat : 카테고리 rawValue 추가

--- EggMeet/EggMeet/SignUp/SignUpMentoAreaSelectionVC.swift
+++ EggMeet/EggMeet/SignUp/SignUpMentoAreaSelectionVC.swift
@@ -22,14 +22,14 @@ class SignUpMentoAreaSelectionVC: UIViewController{
     let mentorDetailAreaKey = "mentorDetailCategory"
     let ud = UserDefaults.standard
     var isCategorySelected: Bool = false
-    let detailCategoryData = [["웹 프론트엔드 개발","백엔드 개발", "모바일 앱 개발", "게임 개발", "DevOps", "머신러닝", "데이터분석", "프로그래밍 언어", "기타"], ["영어","일본어", "중국어", "스페인어", "프랑스어", "독일어", "러시아어", "아랍어","이탈리아어","기타"],
+    let detailCategoryData = [["웹프론트엔드 개발","백엔드 개발", "모바일 앱 개발", "게임 개발", "DevOps", "머신러닝", "데이터분석", "프로그래밍 언어", "기타"], ["영어","일본어", "중국어", "스페인어", "프랑스어", "독일어", "러시아어", "아랍어","이탈리아어","기타"],
     ["취업/이직 코칭", "자기소개서 코칭", "직무 스킬", "기타"],
-    ["국가 공인 자격증", "기타"],["영상/사진 편집", "영상/사진 촬영", "카메라 활용", "기타"],
+    ["국가 공인 자격증", "기타"],["편집", "촬영", "카메라 활용", "기타"],
     ["일러스트","컨셉아트", "이모티콘", "드로잉", "캘리그라피","디자인 툴", "UI/UX", "시각디자인", "기타"],
     ["디지털/SNS 마케팅", "카피라이팅", "데이터 마케팅", "기타"],
     ["회계/세무/재무", "주식 투자", "부동산 투자", "기타"],
-    ["실용음악 이론", "작곡 및 프로듀싱", "작사"],
-    ["여행기", "창업 경험담", "실패 극복 경험", "취업 성공 노하우"]]
+    ["실용음악 이론", "작곡 및 프로듀싱", "작사", "기타"],
+    ["여행기", "창업 경험담", "실패 극복 경험", "취업 성공 노하우", "기타"]]
     var mentorAreaSelectedIndex: Int = 0
     
     override func viewDidLoad() {
@@ -58,7 +58,7 @@ class SignUpMentoAreaSelectionVC: UIViewController{
     }
     
     @IBAction func touchMentorAreaButton(){
-        mentorAreaDD.dataSource = [ "프로그래밍 / 데이터 사이언스","외국어", "취업준비","자격증 / 시험", "사진 / 영상촬영 및 편집", "디자인 및 그래픽", "마케팅", "투자 / 재테크", "경험 / 조언 / 노하우"]
+        mentorAreaDD.dataSource = [ "프로그래밍","외국어", "취업준비","자격증", "사진/영상촬영 및 편집", "디자인", "마케팅", "투자/재테크","작사/작곡", "경험/조언/노하우"]
         mentorAreaDD.textColor = UIColor.black
         mentorAreaDD.selectedTextColor = UIColor.blue
         mentorAreaDD.backgroundColor = UIColor.white
